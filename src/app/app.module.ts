8136f590fedec23cd4dc117b2889fb69a2996c3e
wender Junior <wender.junior@etaure.com.br>
2022-12-08 16:33:00 -0300

    add httpRequest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
